f2d1de28d6c40124a702f8c0e9898859a4987e3a
ericmah123 <emah2@ualberta.ca>
2026-01-19 16:06:27 -0700

    Fix environment variable and routing

--- api/gallery.js
+++ api/gallery.js
@@ -7,7 +7,7 @@ export const config = {
 export default async function handler(req, res) {
     try {
         const { blobs } = await list({
-            token: process.env.VITE_BLOB_READ_WRITE_TOKEN,
+            token: process.env.BLOB_READ_WRITE_TOKEN || process.env.VITE_BLOB_READ_WRITE_TOKEN,
         });
 
         res.status(200).json(blobs);
